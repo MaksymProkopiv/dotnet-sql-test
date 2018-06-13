b5570d469f25b219a9802e3df631e1f52fda014f
Andrii Kasianenko <andrii.kasianenko@lebara.com>
2018-06-13 19:10:47 +0300

    endless loop

--- CoreApp/Program.cs
+++ CoreApp/Program.cs
@@ -5,6 +5,7 @@ using System;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 namespace CoreApp
 {
@@ -22,39 +23,46 @@ namespace CoreApp
             JsonConfigurationProvider jsonConfig = configuration.Providers.FirstOrDefault() as JsonConfigurationProvider;
             EnvironmentVariablesConfigurationProvider envConfig = configuration.Providers.LastOrDefault() as EnvironmentVariablesConfigurationProvider;
 
-            string connStr = string.Empty;
-
-            if (envConfig.TryGet("TestConnection", out connStr))
+            while (true)
             {
-                TestConnection(connStr);
-                return;
-            }
+                string connStr = string.Empty;
 
-            int i = 1;
+                if (envConfig.TryGet("TestConnection", out connStr))
+                {
+                    TestConnection(connStr);
+                    return;
+                }
 
-            while (jsonConfig.TryGet("TestConnection" + i, out connStr))
-            {
-                i++;
+                int i = 1;
+
+                while (jsonConfig.TryGet("TestConnection" + i, out connStr))
+                {
+                    i++;
+
+                    Console.WriteLine("--------------------");
+                    Console.WriteLine("Will test " + connStr);
+                    TestConnection(connStr);
+                }
 
                 Console.WriteLine("--------------------");
-                Console.WriteLine("Will test " + connStr);
-                TestConnection(connStr);
+                Console.WriteLine("And now ENV variables");
+
+                i = 1;
+
+                while (envConfig.TryGet("TestConnection" + i, out connStr))
+                {
+                    i++;
+
+                    Console.WriteLine("--------------------");
+                    Console.WriteLine("Will test " + connStr);
+
+                    TestConnection(connStr);
+                }
+
+
+                Thread.Sleep(new TimeSpan(0, 1, 0));
             }
 
-            Console.WriteLine("--------------------");
-            Console.WriteLine("And now ENV variables");
-
-            i = 1;
-
-            while (envConfig.TryGet("TestConnection" + i, out connStr))
-            {
-                i++;
-
-                Console.WriteLine("--------------------");
-                Console.WriteLine("Will test " + connStr);
-
-                TestConnection(connStr);
-            }
 
             Console.WriteLine("We're done. Press any key to quit.");
             Console.ReadKey();
